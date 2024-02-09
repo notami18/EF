31ac80102b96514f4dc73775cf38ee0755f0a856
Andres Minota <andresnotami@gmail.com>
2024-02-08 20:16:09 -0500

    feat: :sparkles: Utilizando base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using EF;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TaksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbConnection", async ([FromServices] TaksContext dbContext) =>
+{
+  dbContext.Database.EnsureCreated();
+  return Results.Ok($"Connection to the database {dbContext.Database.IsInMemory()} is successful");
+});
 
 app.Run();
