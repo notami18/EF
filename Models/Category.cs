b0c48bbb5285362601fd813aa53e64399dd2ac0b
Andres Minota <minocamc-18@hotmail.com>
2024-02-08 19:41:49 -0500

    feat: :sparkles: Mapeo de modelos usando atributos

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace EF.Models
 {
   public class Category
   {
+    [Key]
     public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(150)]
     public string? Name { get; set; }
     public string? Description { get; set; }
     public virtual ICollection<Task>? Tasks { get; set; }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +9,23 @@ namespace EF.Models
 {
   public class Task
   {
+    [Key]
     public Guid TaskId { get; set; }
-    public Guid CategoydD { get; set; }
+
+    [ForeignKey("CategoryId")]
+    public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string? Title { get; set; }
     public string? Description { get; set; }
     public Priority PriorityTask { get; set; }
     public DateTime Date { get; set; }
 
     public virtual Category? Category { get; set; }
+
+    [NotMapped] // This property will not be mapped to the database
+    public string? Resumen { get; set; }
   }
 
   public enum Priority
